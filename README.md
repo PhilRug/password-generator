7f08dd8d6aabc2b2042c02c7c567a791cbc0e381
PhilRug <ruggiero.pj@gmail.com>
2023-02-11 09:51:07 -0500

    updating JS

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # password-generator
-password generator utilizing javascript
+
+## password generator utilizing javascript
+
+
+Dont you hate it when websites or comanies make you update your password every 90 days? It can be fine at first but eventually you run out of passwords that you can think of! Using javascript, i created a password generator that gives the user full control on how complex their password will be. This is my FIRST attempt at a project using javascript, but the use of prompt and alert buttons was fun. 
--- develop/script.js
+++ develop/script.js
@@ -1,4 +1,8 @@
 // Assignment code here
+var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+var symbols = "!@#$%^&*()";
+var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChars = "0123456789";
 
 
 // Get references to the #generate element
@@ -8,10 +12,55 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  passFinal = parseInt(passLength);
+  
   passwordText.value = password;
+if (confirm("Would you like to randomize a password?") == true) {
+    passLength = prompt("How many characters would you like your password to be?");
+    document.getElementById("password").value = password;
+function randomLower() {
+        lowerChars(Math.floor(Math.random() * lowerChars.length))
+    }
+    lowerChars.confirm("do you want to use lower characters?")
+    if (lowerChars === true) {
+      password.push()
+  }
+}
 
+function randomUpper() {
+    upperChars(Math.floor(Math.random() * upperChars.length))
+}
+
+
+
+if (upperChars === true) {
+    password.push()
+}
+function randomSymbols() {
+    symbols(Math.floor(Math.random() * symbols.length))
+}
+if (symbols === true) {
+    password.push()
+}
+function randomNumber() {
+    numberChars(Math.floor(Math.random() * numberChars.length))
+}
+if (numberChars === true) {
+    password.push()
+}
+}
+for (var i = 0; i <= writePassword; i++) {
+    var generatePassword = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    var passwordString = "";
+}
+
+generateBtn.onclick = function() {
+    prompt("How many characters do you want?")
+}
